3565c843d32e19e858a7789898dc7d0dfb6b67aa
Daniel Shroff <danielshroff@hotmail.com>
2018-01-27 10:09:03 +0530

    added rough client

---
+++ HydroMessage_Client/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace HydroMessage_Client
+{
+    partial class HydroMessage_Client
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.serverIPInput = new System.Windows.Forms.TextBox();
+            this.serverIPLabel = new System.Windows.Forms.Label();
+            this.userNameLabel = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // serverIPInput
+            // 
+            this.serverIPInput.Location = new System.Drawing.Point(59, 12);
+            this.serverIPInput.Name = "serverIPInput";
+            this.serverIPInput.Size = new System.Drawing.Size(213, 20);
+            this.serverIPInput.TabIndex = 0;
+            this.serverIPInput.Text = "127.0.0.1";
+            // 
+            // serverIPLabel
+            // 
+            this.serverIPLabel.AutoSize = true;
+            this.serverIPLabel.Location = new System.Drawing.Point(2, 12);
+            this.serverIPLabel.Name = "serverIPLabel";
+            this.serverIPLabel.Size = new System.Drawing.Size(51, 13);
+            this.serverIPLabel.TabIndex = 1;
+            this.serverIPLabel.Text = "Server IP";
+            this.serverIPLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // userNameLabel
+            // 
+            this.userNameLabel.AutoSize = true;
+            this.userNameLabel.Location = new System.Drawing.Point(2, 45);
+            this.userNameLabel.Name = "userNameLabel";
+            this.userNameLabel.Size = new System.Drawing.Size(60, 13);
+            this.userNameLabel.TabIndex = 2;
+            this.userNameLabel.Text = "User Name";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(59, 45);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(213, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // HydroMessage_Client
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.userNameLabel);
+            this.Controls.Add(this.serverIPLabel);
+            this.Controls.Add(this.serverIPInput);
+            this.Name = "HydroMessage_Client";
+            this.Text = "HydroMessage Client";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox serverIPInput;
+        private System.Windows.Forms.Label serverIPLabel;
+        private System.Windows.Forms.Label userNameLabel;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ HydroMessage_Client/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HydroMessage_Client
+{
+    public partial class HydroMessage_Client : Form
+    {
+        public HydroMessage_Client()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
